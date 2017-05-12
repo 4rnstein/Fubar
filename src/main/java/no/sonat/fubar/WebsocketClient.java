0bd8c4cd0fb79ee648cb5044afab797711470d78
Arnstein <arnsteinx@gmail.com>
2017-05-12 11:20:33 +0200

    sdafsdfsdf

--- src/main/java/no/sonat/fubar/WebsocketClient.java
+++ src/main/java/no/sonat/fubar/WebsocketClient.java
@@ -1,9 +1,100 @@
 package no.sonat.fubar;
 
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Future;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.StatusCode;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
 /**
- * Copyright (c) All Rights Reserved.
- * User: AC30788 | Arnstein Flemmen
- * Date: 12.05.2017
+ * Websocket klient eksempel.
+ *
+ * Hvis du bygger med Maven legg dette i pom.xml:
+ *
+
+
+ *
+ *
+ *
  */
 public class WebsocketClient {
+    public static void main(String[] args) {
+        String destUri = "ws://echo.websocket.org";
+        if (args.length > 0) {
+            destUri = args[0];
+        }
+        WebSocketClient client = new WebSocketClient();
+        SimpleEchoSocket socket = new WebsocketClient.SimpleEchoSocket();
+        try {
+            client.start();
+            URI echoUri = new URI(destUri);
+            ClientUpgradeRequest request = new ClientUpgradeRequest();
+            client.connect(socket, echoUri, request);
+            System.out.printf("Connecting to : %s%n", echoUri);
+            socket.awaitClose(5, TimeUnit.SECONDS);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            try {
+                client.stop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Basic Echo Client Socket
+     */
+    @WebSocket(maxTextMessageSize = 64 * 1024)
+    public static class SimpleEchoSocket {
+
+        private final CountDownLatch closeLatch;
+
+        @SuppressWarnings("unused")
+        private Session session;
+
+        public SimpleEchoSocket() {
+            this.closeLatch = new CountDownLatch(1);
+        }
+
+        public boolean awaitClose(int duration, TimeUnit unit) throws InterruptedException {
+            return this.closeLatch.await(duration, unit);
+        }
+
+        @OnWebSocketClose
+        public void onClose(int statusCode, String reason) {
+            System.out.printf("Connection closed: %d - %s%n", statusCode, reason);
+            this.session = null;
+            this.closeLatch.countDown();
+        }
+
+        @OnWebSocketConnect
+        public void onConnect(Session session) {
+            System.out.printf("Got connect: %s%n", session);
+            this.session = session;
+            try {
+                Future<Void> fut;
+                fut = session.getRemote().sendStringByFuture("Hello");
+                fut.get(2, TimeUnit.SECONDS);
+                session.close(StatusCode.NORMAL, "I'm done");
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+
+        @OnWebSocketMessage
+        public void onMessage(String msg) {
+            System.out.printf("Got msg: %s%n", msg);
+        }
+    }
 }
