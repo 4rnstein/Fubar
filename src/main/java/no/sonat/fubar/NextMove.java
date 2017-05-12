c40bdadff288ccba9856839e1b5eb8140ff16237
Arnstein <arnsteinx@gmail.com>
2017-05-12 13:45:27 +0200

    Merge branch 'master' of https://github.com/4rnstein/Fubar
    # Conflicts:
    #	pom.xml

--- src/main/java/no/sonat/fubar/NextMove.java
+++ src/main/java/no/sonat/fubar/NextMove.java
@@ -1,5 +1,7 @@
 package no.sonat.fubar;
 
+import no.sonat.fubar.model.MazeCell;
+
 /**
  * Copyright (c) All Rights Reserved.
  * User: AC30788 | Arnstein Flemmen
@@ -8,9 +10,13 @@ package no.sonat.fubar;
 public class NextMove {
     public String Action = "NextMove";
     public String North;// = "Wall" or "Open",
-    public String     West;// = "Wall" or "Open",
+    public String West;// = "Wall" or "Open",
     public String East;// = "Wall" or "Open",
     public String South;// = "Wall" or "Open",
     public String ClientId;
     public String GameId;
+
+    public MazeCell getMazeCell() {
+        return new MazeCell(null, North.equals("Open"), South.equals("Open"), West.equals("Open"), East.equals("Open"));
+    }
 }
